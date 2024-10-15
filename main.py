48fcc518dfe049b70b1999ee6349c0d222e27cba
b1art <70599221+b1art@users.noreply.github.com>
2024-10-15 16:54:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ cursor.execute(SQL_QUERY_apps)
 records = cursor.fetchall()
 data = []
 for r in records:
-    data.append([str(r[0]),str(r[1]),str(r[2]),str(r[3]),int(r[4]),int(r[5]),int(r[6])])
+    data.append([str(r[0]),str(r[1]),str(r[2]),str(r[3]),int(r[4]),int(r[5]),int(r[6]),int(r[7])])
 
 
 # for r in records:
@@ -47,7 +47,7 @@ def click_apps():
 
         for item in tree.selection():
             #Label(window_app, text=tuple(tree.item(item)["values"])).pack()
-            app=tree.item(item)["values"][6]
+            app=tree.item(item)["values"][7]
             SQL_QUERY_app = f"""
             Select txtOperationTypeName,txtWorkerSurname,txtWorkerName,txtWorkerSecondName,txtOperationDescription
             From s004.tblOperationType
@@ -55,38 +55,68 @@ def click_apps():
             inner  join s004.tblWorker on s004.tblOperation.intWorkerId = s004.tblWorker.intWorkerId
             where intFlatId = {app}
             """
+
             cursor.execute(SQL_QUERY_app)
             operations = cursor.fetchall()
-            for oper in operations:
-                Label(window_app, text=oper).pack()
 
-        flatid_field = ttk.Entry(window_app)
+            columns_oper = ("txtOperationTypeName", "txtWorkerSurname", "txtWorkerName", "txtWorkerSecondName", "txtOperationDescription")
+            tree_oper = ttk.Treeview(window_app, columns=columns_oper, show="headings")
+            tree_oper.pack(fill=BOTH, expand=0, side=BOTTOM)
+            tree_oper.heading("txtOperationTypeName", text="Тип работ")
+            tree_oper.heading("txtWorkerSurname", text="Фамилия рабочего")
+            tree_oper.heading("txtWorkerName", text="Имя рабочего")
+            tree_oper.heading("txtWorkerSecondName", text="Отчество рабочего")
+            tree_oper.heading("txtOperationDescription", text="Описание")
+
+            data_opers = []
+            for oper in operations:
+                data_opers.append([str(oper[0]), str(oper[1]), str(oper[2]), str(oper[3]), str(oper[4])])
+
+            for oper in data_opers:
+                tree_oper.insert("", END, values=oper)
+
+
         intOperationTypeId_field = ttk.Entry(window_app)
         datOperationDate_field = ttk.Entry(window_app)
         intWorkerId_field = ttk.Entry(window_app)
         txtOperationDescription_field = ttk.Entry(window_app)
 
-        flatid_field.pack(anchor=NW, padx=8, pady=8)
-        intOperationTypeId_field.pack(anchor=NW, padx=8, pady=8)
-        datOperationDate_field.pack(anchor=NW, padx=8, pady=8)
-        intWorkerId_field.pack(anchor=NW, padx=8, pady=8)
-        txtOperationDescription_field.pack(anchor=NW, padx=8, pady=8)
+        Label(window_app, text='Квартира {}'.format(app)).pack(anchor=N)
+
+        Label(window_app, text='ID операции').pack(anchor=W)
+        intOperationTypeId_field.pack(anchor=W)
+        Label(window_app, text='Дата проведения\nxxxx.xx.xx').pack(anchor=W)
+        datOperationDate_field.pack(anchor=W)
+        Label(window_app, text='ID рабочего').pack(anchor=W)
+        intWorkerId_field.pack(anchor=W)
+        Label(window_app, text='Описание работ').pack(anchor=W)
+        txtOperationDescription_field.pack(anchor=W)
+
+
 
         def click_addoper():
-            flat = flatid_field.get()
+            flat = app
             owner = intOperationTypeId_field.get()
             date = datOperationDate_field.get()
+            print(date.replace(".",""))
             worker = intWorkerId_field.get()
             descr = txtOperationDescription_field.get()
 
             SQL_QUERY_addoper = f"""
-                        INSERT s004.tblOperation (FlatId, intOperationTypeId, datOperationDate, intWorkerId, txtOperationDescription)
-                            VALUES ('{flat}', {owner}, {date}, {worker}, {descr});
+                        INSERT INTO s004.tblOperation (intFlatId, intOperationTypeId, datOperationDate, intWorkerId, txtOperationDescription)
+                            VALUES ({flat}, {owner}, '{date}', {worker}, '{descr}');
                         """
             cursor.execute(SQL_QUERY_addoper)
 
+            cursor.execute(SQL_QUERY_app)
+            operations = cursor.fetchall()
+
+            data_opers.append([str(operations[-1][0]), str(operations[-1][1]), str(operations[-1][2]), str(operations[-1][3]), str(operations[-1][4])])
+
+            tree_oper.insert("", END, values=data_opers[-1])
+
         button_addoper = ttk.Button(window_app, text="Добавить работу", command=click_addoper)
-        button_addoper.pack(anchor=NW, expand=1)
+        button_addoper.pack(anchor=W, pady=25)
 
 
 
@@ -101,14 +131,20 @@ def click_apps():
         intCount_field = ttk.Entry(window_addapps)
         intStorey_field = ttk.Entry(window_addapps)
 
+        Label(window_addapps, text='Адрес').pack(anchor=W)
         adress_field.pack(anchor=NW, padx=8, pady=8)
+        Label(window_addapps, text='ID владельца').pack(anchor=W)
         intOwnerId_field.pack(anchor=NW, padx=8, pady=8)
+        Label(window_addapps, text='Площадь').pack(anchor=W)
         fltArea_field.pack(anchor=NW, padx=8, pady=8)
+        Label(window_addapps, text='Количество жильцов').pack(anchor=W)
         intCount_field.pack(anchor=NW, padx=8, pady=8)
+        Label(window_addapps, text='Этаж').pack(anchor=W)
         intStorey_field.pack(anchor=NW, padx=8, pady=8)
 
         def submit():
             adress = adress_field.get()
+            print(adress)
             intOwnerId = intOwnerId_field.get()
             fltArea = fltArea_field.get()
             intCount = intCount_field.get()
@@ -119,6 +155,7 @@ def click_apps():
                 VALUES ('{adress}', {intOwnerId}, {fltArea}, {intCount}, {intStorey});
             """
             cursor.execute(SQL_QUERY_addapps)
+            conn.commit()
 
         buttonsubmit = ttk.Button(window_addapps, text="Подтвердить добавление", command=submit)
         buttonsubmit.pack(anchor=NW, expand=1)
